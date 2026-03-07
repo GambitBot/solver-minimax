c0a201a256c84b44a089ad8ad691f8ffb2c5fb0a
Patrick Kotelko <patrick.dev@kotelko.ca>
2026-03-07 14:05:28 -0600

    Update __init__.py to include relevant classes

--- src/solver/__init__.py
+++ src/solver/__init__.py
@@ -1,3 +1,6 @@
 """Chess solver using a Minimax algorithm"""
 
+from . import board as board
+from . import config as config
+from . import piece as piece
 from .server import run_server as run_server
