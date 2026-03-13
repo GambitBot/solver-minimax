2208628a755acce054b3cfa6d5c35bfb73c98507
Patrick Kotelko <patrick.dev@kotelko.ca>
2026-03-13 00:23:27 -0500

    Added a benchmark for time-limited opening searches

---
+++ benchmarks/bench_openings_timelimit.py
@@ -0,0 +1,47 @@
+"""Benchmark solution module solution times"""
+
+# Run benchmark with "python -u" flag for unbuffered STDOUT
+
+import time
+
+import solver
+
+# Opening FEN states taken from: https://www.365chess.com/chess-openings/
+FEN_OPENINGS = {
+	"start": "rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1",  # Starting position
+	"spanish": "r1bqkbnr/pppp1ppp/2n5/1B2p3/4P3/5N2/PPPP1PPP/RNBQK2R b KQkq - 3 3",  # Ruy Lopez (Spaning Opening)
+	"italian": "r1bqkbnr/pppp1ppp/2n5/4p3/2B1P3/5N2/PPPP1PPP/RNBQK2R b KQkq - 3 3",  # Italian Game
+	"scotch": "r1bqkbnr/pppp1ppp/2n5/1B2p3/4P3/5N2/PPPP1PPP/RNBQK2R b KQkq - 3 3",  # Scotch Game
+	"sicilian": "rnbqkbnr/pp1ppppp/8/2p5/4P3/8/PPPP1PPP/RNBQKBNR w KQkq c6 0 2",  # Sicilian Defence
+	"french": "rnbqkbnr/ppp2ppp/4p3/3p4/3PP3/8/PPP2PPP/RNBQKBNR w KQkq d6 0 3",  # French Defence
+	"scandanavian": "rnbqkbnr/ppp1pppp/8/3p4/4P3/8/PPPP1PPP/RNBQKBNR w KQkq d6 0 2",  # Scandanavian Defence
+	"petrov": "rnbqkb1r/pppp1ppp/5n2/4p3/4P3/5N2/PPPP1PPP/RNBQKB1R w KQkq - 2 3",  # Petrov's Defence
+	"philidor": "rnbqkbnr/ppp2ppp/3p4/4p3/4P3/5N2/PPPP1PPP/RNBQKB1R w KQkq - 0 3",  # Philidor Defence
+	"kings_gambit": "rnbqkbnr/ppp1pppp/8/3p4/4P3/8/PPPP1PPP/RNBQKBNR w KQkq d6 0 2",  # King's Gambit
+	"caro-kann": "rnbqkbnr/pp1ppppp/2p5/8/4P3/8/PPPP1PPP/RNBQKBNR w KQkq - 0 2",  # Caro-Kann Defence
+	"modern": "rnbqkbnr/pppppp1p/6p1/8/4P3/8/PPPP1PPP/RNBQKBNR w KQkq - 0 2",  # Modern Defence
+	"pirc": "rnbqkbnr/ppp1pppp/3p4/8/4P3/8/PPPP1PPP/RNBQKBNR w KQkq - 0 2",  # Pirc Defence
+	"owen": "rnbqkbnr/p1pppppp/1p6/8/4P3/8/PPPP1PPP/RNBQKBNR w KQkq - 0 2",  # Owen's Defence
+	"alekhine": "rnbqkb1r/pppppppp/5n2/8/4P3/8/PPPP1PPP/RNBQKBNR w KQkq - 1 2",  # Alekhine Defence
+	"vienna": "rnbqkbnr/pppp1ppp/8/4p3/4P3/2N5/PPPP1PPP/R1BQKBNR b KQkq - 1 2",  # Vienna Game
+	"ponziani": "r1bqkbnr/pppp1ppp/2n5/4p3/4P3/2P2N2/PP1P1PPP/RNBQKB1R b KQkq - 0 3",  # Ponziani Opening
+	"queens-gambit": "rnbqkbnr/ppp1pppp/8/3p4/2PP4/8/PP2PPPP/RNBQKBNR b KQkq c3 0 2",  # Queen's Gambit
+	"grunfeld": "rnbqkb1r/ppp1pp1p/5np1/3p4/2PP4/2N5/PP2PPPP/R1BQKBNR w KQkq d6 0 4",  # Grunfeld Defence
+	"budapest": "rnbqkb1r/pppp1ppp/5n2/4p3/2PP4/8/PP2PPPP/RNBQKBNR w KQkq e6 0 3",  # Budapest Gambit
+}
+
+
+print("Gambit Solution Module Performance Benchmark")
+board = solver.board.Board()
+start_time = 0.0
+print("Opening states - time limited")
+depth = 6
+for k in FEN_OPENINGS:
+	print(f"{k}", end="")
+	board.reset()
+	board.load_fen_string(FEN_OPENINGS[k], True)
+	start_time = time.time()
+	_, depth = board.solve(6, 3, 4.5)
+	result = time.time() - start_time
+	print(f",{result},{depth}", end="")
+	print("")  # Make sure we have a line break
